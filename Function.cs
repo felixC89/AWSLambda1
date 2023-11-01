1892e8c950d9c5ffe900ed3ecb42ffbe7b93a140
felix calderon <felix.calderon89@hotmail.com>
2023-11-01 12:33:02 -0600

    Adding Fluent Validation

--- Function.cs
+++ Function.cs
@@ -1,6 +1,8 @@
 using Amazon.Lambda.Core;
 using AWSLambda1.RequestModel;
 using AWSLambda1.ResponseModel;
+using AWSLambda1.Validators;
+using FluentValidation.Results;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -18,6 +20,15 @@ namespace AWSLambda1
         /// <returns></returns>
         public async Task<ResponseSaludo> FunctionSaludo(RequestSaludo input, ILambdaContext context)
         {
+            ValidatorSaludo validator = new ValidatorSaludo();
+
+            ValidationResult result = validator.Validate(input);
+
+            if (result.IsValid != true) 
+            {
+                return new ResponseSaludo() { ErrorCode = -1 , ErrorMessage = $"Error de validación: {result?.Errors.FirstOrDefault()}" };
+            }
+
             var response = new ResponseSaludo();
 
             try
@@ -25,7 +36,7 @@ namespace AWSLambda1
                 response.ErrorCode = 0;
                 response.ErrorMessage = $"Hola {input.key1} {input.key2} {input.key3} {input.key4}, FunctionName: {context.FunctionName}";
             }
-            catch (Exception e) 
+            catch (Exception e)
             {
                 response.ErrorCode = -1;
                 response.ErrorMessage = $"Ocurrió un error al correr la funcion lambda: {e.Message}";
